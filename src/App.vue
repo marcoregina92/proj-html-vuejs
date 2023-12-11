fa4b074e2aac2ed64eab6602ad6558985bf21faa
Marco <marcoregina92@gmail.com>
2023-12-11 16:32:02 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script>
+import AppHeader from "./components/AppHeader.vue"
+
+import { store } from "./store.js" //state management
+
+export default {
+  components: {
+    AppHeader
+  },
+  data() {
+    return {
+      store
+    }
+  },
+  mounted() {
+
+  },
+  methods: {
+  }
+}
+</script>
+
+<template>
+  <main>
+    <AppHeader />
+  </main>
+</template>
+
+
+<style scoped></style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,26 @@
+<script>
+import { store } from "../store" //state management
+
+export default {
+    name: "AppHeader",
+    data() {
+        return {
+            store
+        }
+    },
+    methods: {
+
+    },
+    mounted() {
+
+    }
+}
+</script>
+
+<template>
+    <div>
+        <h1>Sono un componente!</h1>
+    </div>
+</template>
+
+<style scoped></style>
