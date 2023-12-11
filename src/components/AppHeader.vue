0e14a42e2ab3491964a3e1ec4ca0c53c92bffdc7
Marco <marcoregina92@gmail.com>
2023-12-11 17:12:23 +0100

    Create AppHeader components

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -18,9 +18,46 @@ export default {
 </script>
 
 <template>
-    <div>
-        <h1>Sono un componente!</h1>
-    </div>
+    <section class="">
+        <div class="headerTop">
+            <div class="d-flex align-center">
+                <i class="fas fa-phone iconCell"></i>
+                <p class="m-0">(001) 88451234 88455438</p>
+            </div>
+
+            <div class="d-flex align-center">
+                <ul class="socialIcon m-0">
+                    <li><i class="fab fa-facebook-f"></i></li>
+                    <li><i class="fab fa-twitter"></i></li>
+                    <li><i class="fab fa-pinterest"></i></li>
+                </ul>
+            </div>
+        </div>
+    </section>
 </template>
 
-<style scoped></style>
+<style scoped>
+.headerTop {
+    color: white;
+    display: flex;
+    background-color: #3c3c3c;
+    justify-content: space-between;
+    padding: 20px;
+    align-items: center;
+}
+
+.socialIcon {
+    list-style-type: none;
+    display: flex;
+}
+
+li {
+    margin-right: 20px;
+    font-size: 20px;
+}
+
+.iconCell {
+    font-size: 20px;
+    margin-right: 20px;
+}
+</style>
