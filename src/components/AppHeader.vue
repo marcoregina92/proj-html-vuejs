3d119c5fae39c325d50d952124299e3513e3c008
Marco <marcoregina92@gmail.com>
2023-12-11 17:59:39 +0100

    Inserisce Header bottom

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -7,6 +7,7 @@ export default {
         return {
             store
         }
+
     },
     methods: {
 
@@ -18,7 +19,7 @@ export default {
 </script>
 
 <template>
-    <section class="">
+    <section>
         <div class="headerTop">
             <div class="d-flex align-center">
                 <i class="fas fa-phone iconCell"></i>
@@ -31,6 +32,28 @@ export default {
                     <li><i class="fab fa-twitter"></i></li>
                     <li><i class="fab fa-pinterest"></i></li>
                 </ul>
+            </div>
+        </div>
+    </section>
+
+    <section>
+        <div class="headerBottom d-flex">
+            <img src="../assets/img/cropped-Group-39-2x.png" alt="">
+
+            <div class="d-flex">
+                <ul class="menuNav m-0">
+                    <li><a href=""> HOME </a></li>
+                    <li><a href=""> ABOUT </a></li>
+                    <li><a href=""> SERVICES </a></li>
+                    <li><a href=""> SHOWCASE </a></li>
+                    <li><a href=""> BLOG </a></li>
+                    <li><a href=""> CONTACT </a></li>
+                </ul>
+            </div>
+
+            <div>
+                <button>PURCHASE</button>
+                <i class="fas fa-search search"></i>
             </div>
         </div>
     </section>
@@ -51,7 +74,7 @@ export default {
     display: flex;
 }
 
-li {
+.socialIcon li {
     margin-right: 20px;
     font-size: 20px;
 }
@@ -59,5 +82,40 @@ li {
 .iconCell {
     font-size: 20px;
     margin-right: 20px;
+}
+
+.headerBottom {
+    padding: 20px;
+    display: flex;
+    justify-content: space-between;
+}
+
+
+.menuNav {
+    list-style-type: none;
+    display: flex;
+}
+
+.menuNav a {
+    text-decoration: none;
+    color: #3c3c3c
+}
+
+.menuNav li {
+    margin-right: 20px;
+    align-self: center;
+}
+
+button {
+    color: white;
+    font-size: 13px;
+    padding: 15px;
+    background: rgb(140, 225, 100);
+    background: linear-gradient(90deg, rgba(140, 225, 100, 1) 0%, rgba(78, 221, 129, 1) 30%, rgba(16, 218, 160, 1) 100%);
+    width: 150px;
+    border-style: none;
+    border-radius: 50px;
+    margin-right: 30px;
+    cursor: pointer;
 }
 </style>
