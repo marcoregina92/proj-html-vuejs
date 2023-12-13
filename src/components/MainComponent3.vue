3d08e8a7ced2bb53f962d64bc23675de0f07940f
Marco <marcoregina92@gmail.com>
2023-12-13 18:28:49 +0100

    add hover animation section3

--- src/components/MainComponent3.vue
+++ src/components/MainComponent3.vue
@@ -37,23 +37,72 @@ export default {
     </div>
 
     <div class="myContainer">
-        <div class="box-card">
-            <div class="mycard" v-for="design in store.designs">
-                <div class="mask mask d-flex justify-content-end flex-column p-4">
-                    <h4 class="text-white"> {{ design.name }}</h4>
+        <div class="box-card ">
+            <div class="mycard " v-for="design in store.designs">
+                <div class="mask d-flex justify-content-end flex-column p-4 ">
+                    <h4 class="text-white slideUp"> {{ design.name }}</h4>
                     <div class="d-flex text-uppercase">
-                        <h5 class="me-2 text-white">{{ design.genre1 }}</h5>
-                        <h5 class="text-white">{{ design.genre2 }}</h5>
+                        <h5 class="me-2 text-white slideUp">{{ design.genre1 }}</h5>
+                        <h5 class="text-white slideUp">{{ design.genre2 }}</h5>
                     </div>
                 </div>
                 <img :src="'../src/assets/img/' + design.image">
             </div>
+        </div>
+
+        <div class="d-flex justify-content-center m-5">
+            <button class="btn text-uppercase">Read More</button>
         </div>
     </div>
 </template>
 
 
 <style scoped>
+button {
+    color: white;
+    font-size: 13px;
+    padding: 15px;
+    background: linear-gradient(90deg, rgba(140, 225, 100, 1) 0%, rgba(78, 221, 129, 1) 30%, rgba(16, 218, 160, 1) 100%);
+    width: 150px;
+    border-style: none;
+    border-radius: 50px;
+    cursor: pointer;
+    box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+}
+
+
+h4 {
+    font-weight: 100;
+}
+
+.mask:hover .slideUp {
+    animation-name: slideInUp;
+    animation-duration: 2s;
+    animation-fill-mode: both;
+}
+
+@keyframes slideInUp {
+    0% {
+        transform: translateY(100%);
+        visibility: visible;
+    }
+
+    100% {
+        transform: translateY(0);
+    }
+}
+
+× @keyframes slide-top {
+    0% {
+        transform: translateY(0);
+    }
+
+    100% {
+        transform: translateY(-100px);
+    }
+}
+
+
 .myContainer {
     width: 90%;
     margin: 0 auto;
